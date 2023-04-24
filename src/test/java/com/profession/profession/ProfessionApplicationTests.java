a7e5aca3e0faf7a1e880753ee0b74266977bede3
alex <apguzman@stefanini.com>
2023-04-24 10:59:42 -0500

    Initial commit

---
+++ src/test/java/com/profession/profession/ProfessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profession.profession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
